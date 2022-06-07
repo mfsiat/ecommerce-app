e14fdd8e66d68fe3e51757d83cc9076f254d5756
Nasirul Islam <nasirul.islam@northsouth.edu>
2022-06-07 21:18:25 +0600

    fetch data from sanity

--- ecommerce/lib/client.js
+++ ecommerce/lib/client.js
@@ -4,7 +4,7 @@ import imageUrlBuilder from "@sanity/image-url";
 export const client = sanityClient({
   projectId: '4yugyspa',
   dataset: 'production',
-  apiVersion: '1.0.0',
+  apiVersion: '1',
   useCdn: true,
   token: process.env.NEXT_PUBLIC_SANITY_TOKEN
 })
--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -1,21 +1,38 @@
 import React from 'react'
 import { FooterBanner, HeroBanner } from '../components'
+import { client } from '../lib/client'
 
 
-const index = () => {
+const index = ({ products, bannerData }) => {
   return (
     <>
-      <HeroBanner />
+      <HeroBanner HeroBanner={bannerData.length && bannerData[0]} />
+      {/* {console.log(bannerData)} */}
       <div className='products-heading'>
         <h2>Best Selling Products</h2>
         <p>Speakers of Many Variations</p>
       </div>
       <div className='products-container'>
-        {['Product 1', 'Product 2'].map((product) => product)}
+        {products?.map((product) => product.name)}
       </div>
       <FooterBanner />
     </>
   )
+}
+
+// fetch data from sanity
+export const getServerSideProps = async () => {
+  // fetch product query
+  const query = '*[_type == "product"]'
+  const products = await client.fetch(query)
+
+  // fetch product query
+  const bannerQuery = '*[_type == "banner"]'
+  const bannerData = await client.fetch(bannerQuery)
+
+  return {
+    props: { products, bannerData }
+  }
 }
 
 export default index
